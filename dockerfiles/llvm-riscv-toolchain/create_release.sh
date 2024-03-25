0df0c85640ec7086c054f2973966b3d6b99d5334
Ford <cameronfdurbin@gmail.com>
2024-03-25 02:17:32 -0700

    remove release script

--- dockerfiles/llvm-riscv-toolchain/create_release.sh
+++
@@ -1,5 +0,0 @@
-docker buildx build                                  \
-    --push                                           \
-    --platform linux/arm64,linux/amd64               \
-    --tag platinumcd/llvm-riscv-toolchain:dev-latest \
-    .
