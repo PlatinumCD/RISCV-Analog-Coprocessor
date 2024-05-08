292e9e865967c67a7ec0ccb3f8eb34531088df26
ford <cfd@yellow.local>
2024-05-08 01:16:10 -0700

    Add updated dockerfiles and makefiles

--- dockerfiles/llvm-riscv-toolchain-dev/tests/mvm_intrinsics.cpp
+++
@@ -1,8 +0,0 @@
-
-int main() {
-    __asm__ __volatile__ ("mvm.set x0, x0, x0");
-    __asm__ __volatile__ ("mvm.l   x0, x0, x0");
-    __asm__ __volatile__ ("mvm     x0, x0, x0");
-    __asm__ __volatile__ ("mvm.s   x0, x0, x0");
-    __asm__ __volatile__ ("mvm.mv  x0, x0, x0");
-}
